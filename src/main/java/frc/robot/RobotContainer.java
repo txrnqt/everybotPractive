5430e2f60c5b0cd212d48970dd17d961cbe6a8f6
jace rodgers <jaransas2008@gmail.com>
2024-12-02 20:41:23 -0600

    intergrated rumbel

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -65,7 +67,14 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         tank.setDefaultCommand(tank.tankCMD(driver));
-        operator.povDown().onTrue(leds.call().withTimeout(5));
+
+
+        /** operator controlls */
+        operator.povDown().onTrue(leds.call().andThen(Commands.startEnd(() -> {
+            operator.getHID().setRumble(RumbleType.kBothRumble, 1);
+        }, () -> {
+            operator.getHID().setRumble(RumbleType.kBothRumble, 0);
+        })).withTimeout(5));
         operator.a().onTrue(hatch.homePosition());
         operator.x().onTrue(hatch.intake());
     }
--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
